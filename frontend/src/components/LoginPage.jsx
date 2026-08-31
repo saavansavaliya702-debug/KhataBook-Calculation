2036e64fdcb80487656a592f06cefbb392ecae27
SavanSavaliya702 <saavansavaliya702@gmail.com>
2026-08-31 14:35:11 +0530

    Update LoginPage.jsx

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -26,7 +26,7 @@ const Login = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://khatabook-calculation.onrender.com/api/auth/login",
         formData,
       );
 
